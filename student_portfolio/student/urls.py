952caebc5fd6617326e97fa5c0fc283ee547a66f
saxonhell <saxonhell@mail.ru>
2024-09-30 14:57:33 -0500

    refactor code

--- student_portfolio/student/urls.py
+++ student_portfolio/student/urls.py
@@ -9,14 +9,10 @@ urlpatterns = [
     path('all', views.review_list, name='review_list'),
     path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('movies/', views.movie_list, name='movie_list'),
-    path('movies/add/', views.add_movie, name='add_movie'),
-    path('reviews/add/', views.add_review, name='add_review'),
     path('discussions/', views.discussion_list, name='discussion_list'),
-    path('discussions/add/', views.add_discussion, name='add_discussion'),
     path('discussions/<int:discussion_id>/', views.discussion_detail, name='discussion_detail'),
     path('crud/', handle_crud_operations, name='crud_operations'),
     path('Fdirector', views.most_discussed_director, name='most_discussed_director'),
-    path('directors/add/', views.add_director, name='add_director'),
     path('directors/', views.director_list, name='director_list'),
 
 ]
--- student_portfolio/student/views.py
+++ student_portfolio/student/views.py
@@ -12,16 +12,6 @@ def director_list(request):
     directors = Director.objects.all()
     return render(request, 'director_list.html', {'directors': directors})
 
-def add_director(request):
-    if request.method == 'POST':
-        form = DirectorForm(request.POST)
-        if form.is_valid():
-            form.save()  # Сохраняем нового режиссёра в базе данных
-            return redirect('director_list')  # Перенаправляем на список режиссёров (создадим его позже)
-    else:
-        form = DirectorForm()
-    return render(request, 'add_director.html', {'form': form})
-
 def most_discussed_director(request):
     start_date_str = request.GET.get('start_date', '')
     end_date_str = request.GET.get('end_date', '')
@@ -140,150 +130,12 @@ def handle_crud_operations(request):
         objects = model_class.objects.all()
         return render(request, 'crud_list.html', {'objects': objects, 'model_name': model_name})
 
-
-
-# Представление для создания обсуждения
-def add_discussion(request):
-    if request.method == 'POST':
-        discussion_form = DiscussionForm(request.POST)
-        if discussion_form.is_valid():
-            discussion = discussion_form.save(commit=False)
-            # Получаем введённое имя пользователя
-            user_name = discussion_form.cleaned_data['user_name']
-
-            # Проверяем, существует ли пользователь с таким именем
-            user, created = User.objects.get_or_create(username=user_name)
-
-            # Устанавливаем пользователя в поле user
-            discussion.user = user
-
-            discussion.save()
-            return redirect('discussion_list')  # Перенаправление после успешного сохранения
-    else:
-        discussion_form = DiscussionForm()
-
-    return render(request, 'add_discussion.html', {'discussion_form': discussion_form})
-
-
-# Представление для создания комментария в обсуждении
-def add_comment(request, discussion_id):
-    discussion = Discussion.objects.get(pk=discussion_id)
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-
-            # Получаем имя пользователя из формы
-            user_name = comment_form.cleaned_data['user_name']
-
-            # Ищем или создаем пользователя
-            user, created = User.objects.get_or_create(username=user_name)
-
-            # Привязываем пользователя к комментарию
-            comment.user = user
-            comment.discussion = discussion  # Связываем комментарий с обсуждением
-            comment.save()
-            return redirect('discussion_detail', pk=discussion.pk)  # Перенаправление после сохранения
-    else:
-        comment_form = CommentForm()
-
-    return render(request, 'discussion_detail.html', {'form': comment_form, 'discussion': discussion})
-
-
-def edit_movie(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
-    if request.method == 'POST':
-        form = MovieForm(request.POST, instance=movie)
-        if form.is_valid():
-            form.save()
-            return redirect('movie_list')
-    else:
-        form = MovieForm(instance=movie)
-    return render(request, 'edit_movie.html', {'form': form, 'movie': movie})
-
-def delete_movie(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
-    if request.method == 'POST':
-        movie.delete()
-        return redirect('movie_list')
-    return render(request, 'delete_movie.html', {'movie': movie})
-
-def edit_review(request, review_id):
-    review = get_object_or_404(Review, id=review_id)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            return redirect('movie_list')
-    else:
-        form = ReviewForm(instance=review)
-    return render(request, 'edit_review.html', {'form': form, 'review': review})
-
-def delete_review(request, review_id):
-    review = get_object_or_404(Review, id=review_id)
-    if request.method == 'POST':
-        review.delete()
-        return redirect('movie_list')
-    return render(request, 'delete_review.html', {'review': review})
-
-def edit_discussion(request, discussion_id):
-    discussion = get_object_or_404(Discussion, id=discussion_id)
-    if request.method == 'POST':
-        form = DiscussionForm(request.POST, instance=discussion)
-        if form.is_valid():
-            form.save()
-            return redirect('discussion_list')
-    else:
-        form = DiscussionForm(instance=discussion)
-    return render(request, 'edit_discussion.html', {'form': form, 'discussion': discussion})
-
-def delete_discussion(request, discussion_id):
-    discussion = get_object_or_404(Discussion, id=discussion_id)
-    if request.method == 'POST':
-        discussion.delete()
-        return redirect('discussion_list')
-    return render(request, 'delete_discussion.html', {'discussion': discussion})
-
-
-def edit_comment(request, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-
-    if request.method == 'POST':
-        form = CommentForm(request.POST, instance=comment)
-
-        if form.is_valid():
-            # Получаем введённое имя пользователя из формы
-            user_name = form.cleaned_data['user_name']
-
-            # Ищем пользователя или создаём нового, если его нет
-            user, created = User.objects.get_or_create(username=user_name)
-
-            # Привязываем пользователя к комментарию
-            comment.user = user
-            form.save()  # Сохраняем комментарий с новым пользователем
-
-            return redirect('discussion_detail', discussion_id=comment.discussion.id)
-    else:
-        # Передаем текущее имя пользователя в поле user_name при рендеринге формы
-        form = CommentForm(instance=comment, initial={'user_name': comment.user.username if comment.user else ''})
-
-    return render(request, 'edit_comment.html', {'form': form, 'comment': comment})
-
-
-def delete_comment(request, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-    if request.method == 'POST':
-        comment.delete()
-        return redirect('discussion_detail', discussion_id=comment.discussion.id)
-    return render(request, 'delete_comment.html', {'comment': comment})
-
-
-# Список обсуждений
 def discussion_list(request):
     discussions = Discussion.objects.all()
     return render(request, 'discussion_list.html', {'discussions': discussions})
 
-# Детали обсуждения и комментарии
+
+# Список обсуждений
 def discussion_detail(request, discussion_id):
     discussion = get_object_or_404(Discussion, id=discussion_id)
     comments = discussion.comments.all()
@@ -311,34 +163,8 @@ def discussion_detail(request, discussion_id):
     return render(request, 'discussion_detail.html', {'discussion': discussion, 'comments': comments, 'form': form})
 
 
-# Представление для добавления фильма
-def add_movie(request):
-    if request.method == 'POST':
-        form = MovieForm(request.POST)
-        if form.is_valid():
-            form.save()  # Сохраняем фильм в базу данных
-            return redirect('movie_list')  # Перенаправляем на список фильмов
-    else:
-        form = MovieForm()
-    return render(request, 'add_movie.html', {'form': form})
 
 # Представление для добавления отзыва
-def add_review(request):
-    if request.method == 'POST':
-        review_form = ReviewForm(request.POST)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            user_name = review_form.cleaned_data['user_name']
-            user, created = User.objects.get_or_create(username=user_name)
-            review.user = user
-            review.save()
-            return redirect('movie_detail', movie_id=review.movie.id)  # Здесь передаем movie_id
-    else:
-        review_form = ReviewForm()
-
-    return render(request, 'add_review.html', {'review_form': review_form})
-
-# Представление для списка фильмов с отзывами
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'movie_list.html', {'movies': movies})
